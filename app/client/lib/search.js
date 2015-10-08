38d179b7a28913caa9d92be4bd52f1d765de1fd0
Alex Beauchemin <abeauchemin@u92.com>
2015-10-08 16:38:32 -0400

    Link to twitch channel Add song in 2 steps (to be completed)

---
+++ app/client/lib/search.js
@@ -0,0 +1,21 @@
+App.search = (function () {
+    var Search = {
+        /**
+         * Private Functions
+         */
+        test() {
+
+        }
+
+
+        /**
+         * Public functions
+         */
+
+
+    };
+
+    return {
+        test: Search.text.bind(Search)
+    };
+})();
